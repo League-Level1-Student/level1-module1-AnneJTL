89b906eac3357223bd7ede7c30d994bed6d566bd
AnneJTL <anne.chatin@jointheleague.org>
2018-09-17 14:35:20 -0700

    Harry Runner

---
+++ src/HarryRunner.java
@@ -0,0 +1,24 @@
+
+public class HarryRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Create a HarryRunner class with a main method that:
+		
+		// 1. makes harry potter
+		HarryPotter harry = new HarryPotter();
+		
+		// 2. become invisible
+		harry.makeInvisible(true);
+		
+		// 3. spy on Professor Snape
+		harry.spyOnSnape();
+		
+		// 4. become visible again
+		harry.makeInvisible(true);
+		
+		// 5. cast a â€œstupefyâ€� spell
+		harry.castSpell("â€œstupefyâ€�");
+	}
+
+}
