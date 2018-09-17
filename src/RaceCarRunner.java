c5db08155ab610ceb83f44e96f04717c7e097a66
AnneJTL <anne.chatin@jointheleague.org>
2018-09-17 14:26:35 -0700

    Race Car Runner

--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -5,14 +5,22 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		RaceCar f1 = new RaceCar("Renault", 5);
+		
 		// 2. Print the RaceCar's position in the race
+		System.out.println("Tu es en position " + f1.getPositionInRace());
 
 		// 3. Crash the RaceCar
+		f1.crash();
 		
 		// 4. If the car is damaged. Bring it in for a pit stop.
+		if (f1.isDamaged())
+			f1.pit();
 
 		// 5. Help the car move into first place.
-
+		f1.overtake();
+		f1.overtake();
+		f1.overtake();
+		f1.overtake();
 	}
 }
