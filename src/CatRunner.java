9615482afeb2ee9e63dd5148b082df420f1c7b36
AnneJTL <anne.chatin@jointheleague.org>
2018-09-17 14:30:58 -0700

    CatRunner

---
+++ src/CatRunner.java
@@ -0,0 +1,22 @@
+
+public class CatRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		// Create a CatRunner class with a main method that creates a Cat object.
+		Cat Horace = new Cat("Horace");
+
+        // 1. Make the Cat meow
+		Horace.meow();
+		
+		// 2. Get the Cat to print it's name
+		Horace.printName();
+
+		// 3. Kill the Cat!
+		Horace.kill();
+
+	}
+
+}
